e0f7538464bfe12c54129ddbb8f1c7be9f92f544
Martin - Danecca account <martin.roger@danecca.com>
2018-12-02 02:36:36 +0000

    Added Thermocouple reading example
    MAX31855 has issues
    MAX6675 works

---
+++ Max31855_read/Max31855_read.ino
@@ -0,0 +1,35 @@
+#include <max6675.h>
+
+//#include <Adafruit_MAX31855.h>
+
+
+#include <SPI.h>
+
+
+#define MAXDO 50 //corresponds to MISO output on header, connects to DO
+#define MAXCLK 52 //clock on ICSP header to CLK
+#define SS 6 //Slave select from D6 to CS
+
+//create thermocouple object
+
+//Adafruit_MAX31855 thermocouple(MAXCLK,SS,MAXDO);
+MAX6675 thermocouple(MAXCLK,SS,MAXDO);
+
+void setup() {
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+  delay(1000);
+
+}
+
+void loop() {
+//  Serial.print("Measured: ");
+//  Serial.println(thermocouple.readInternal());
+//  Serial.print("C=");
+   Serial.println(thermocouple.readCelsius());
+  //Serial.println(thermocouple.readError());
+
+  delay(500);
+  // put your main code here, to run repeatedly:
+
+}
